eb0f2bb64f3243ab78151b4d70f7b57a5609e6f6
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-12-04 18:25:15 +0700

    Initial commit

---
+++ Demo8.Test/GreeterTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo8.Test
+{
+    [TestClass]
+    public class GreeterTest
+    {
+        [TestMethod]
+        public void TestGreet()
+        {
+            const string testName = "FPoly";
+            Greeter greeter = new Greeter();
+
+            StringAssert.Contains(greeter.Greet(testName), testName);
+        }
+
+        /// <summary>
+        /// Checks the behavior of the method with an invalid argument (null).
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestGreetNull()
+        {
+            Greeter greeter = new Greeter();
+            greeter.Greet(null);
+        }
+
+        /// <summary>
+        /// Checks the behavior of the method with an invalid argument (empty).
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestGreetEmpty()
+        {
+            Greeter greeter = new Greeter();
+            greeter.Greet(string.Empty);
+        }
+    }
+}
---
+++ Demo8/Greeter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo8
+{
+    public class Greeter
+    {
+        public string Greet(string name)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException("name");
+            }
+
+            if (name.Trim() == string.Empty)
+            {
+                throw new ArgumentException("name is empty", "name");
+            }
+
+            return $"Hello, {name}";
+        }
+    }
+}
